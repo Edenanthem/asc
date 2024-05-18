1a47fcacb259ae2ce52b69e03e7d9e491c9fba76
Edenanthem <hikakinofficial147@googlemail.com>
2024-05-18 14:35:43 +0900

    Add files via upload

---
+++ stock.py
@@ -0,0 +1,12 @@
+stock = {
+"monutella:wewputikhuwaghack123",
+"mrlitwak:Shawlee113014",
+"xdvenom445:Parth_90335",
+"myatlantiswefa11:Rohith$2038",
+"Alexsays084:kINBAS084",
+"bhargavhuh:9207144045oP",
+"yaargabe:Gabe0912",
+"hoang270697:Hoangga1234",
+"Haipham1930:quanghaipro191",
+"bournvitasnorter:Iam@8447"
+}
